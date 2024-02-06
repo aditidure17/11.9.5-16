448ddbdc4542462264c339d9e4642ab23e2c88fb
aditidure17 <155630429+aditidure17@users.noreply.github.com>
2024-02-06 10:30:46 +0530

    Add files via upload

--- a3/codes/code.c
+++ a3/codes/code.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
-void linespace(int start, int stop, int step, int* n_values, double* x_values, double* y_values, int num_values) {
+void linespace(int start, int stop, int step, int* n_values, double* x_values, int num_values) {
     for (int i = 0; i < num_values; ++i) {
         n_values[i] = start + i * step;
-        // Let the values of y(0) be 5 and d_y be 10.
-        y_values[i] = 5 + n_values[i]*10; 
-        //corresponding values of x(0) = 8.0/15 and d_x = 38.0/75
-        x_values[i] = 8.0/15 + n_values[i]*38.0/75;
+        //corresponding values of x(0) = 36.0/35 and d_x = 82.0/105
+        x_values[i] = 36.0/35 + n_values[i]*82.0/105;
     }
 }
 
@@ -23,17 +21,16 @@ int main() {
     // Allocate arrays to store the generated values
     int n_values[num_values];
     double x_values[num_values];
-    double y_values[num_values];
 
     // Call the linespace function
-    linespace(start, stop, step, n_values, x_values, y_values, num_values);
+    linespace(start, stop, step, n_values, x_values, num_values);
 
     // Save data to a file
     FILE* file = fopen("output.dat", "w");
 
     if (file != NULL) {
         for (int i = 0; i < num_values; ++i) {
-            fprintf(file, "%d %.2lf %.2lf\n", n_values[i], x_values[i], y_values[i]);
+            fprintf(file, "%d %.2lf\n", n_values[i], x_values[i]);
         }
 
         fclose(file);
--- a3/codes/python.py
+++ a3/codes/python.py
@@ -7,7 +7,6 @@ data = np.loadtxt("output.dat")
 # Extract n_values and y_values from the data
 n_values = data[:, 0].astype(int)
 x_values = data[:, 1]
-y_values = data[:, 2].astype(int)
 
 # Create a stem plot
 plt.stem(n_values, x_values, linefmt='|', markerfmt='o', basefmt='b', label='Stem Plot')
@@ -20,14 +19,4 @@ plt.legend()
 plt.savefig('../figs/fig1.png')
 plt.show()
 
-# Create a stem plot
-plt.stem(n_values, y_values, linefmt='|', markerfmt='o', basefmt='b', label='Stem Plot')
-
-plt.xlabel('n_values')
-plt.ylabel('y_values')
-plt.title('Stem Plot of n_values vs y_values')
-plt.grid(True)
-plt.legend()
-plt.savefig('../figs/fig2.png')
-plt.show()
 
